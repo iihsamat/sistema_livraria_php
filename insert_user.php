9d208da805d02a83cbd5769e29895088ad2f6090
samat <e107ti@etec.sp.gov.br>
2024-04-15 20:38:03 -0300

    sistema livraria

--- insert_user.php
+++ insert_user.php
@@ -1,13 +1,21 @@
 <?php
+//Recebendo dados do formulario
+
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $setor = $_POST['setor'];
 
-echo "$nome";
+//Inserindo banco de dados
 
+include 'conexao.php';
 
+//Dados para inserir os dados
+$insert = "INSERT INTO tb_user VALUES (NULL,'$nome','$email','$senha','$setor')";
 
+//inserindo os dados no banco de dados utilizando a função mysqli
+$query = mysqli_query($conexao, $insert);
 
+echo "Inserido com Sucesso";
 
 ?>
